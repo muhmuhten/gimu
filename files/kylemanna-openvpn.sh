053c6613328acee5cea57353802c159aa572b8e1
Muh Muhten <muh.muhten@gmail.com>
2015-10-23 15:24:54 -0400

    files/kylemanna-openvpn.sh: don't assume we're running from the host
    docker may be running over the network

--- files/kylemanna-openvpn.sh
+++ files/kylemanna-openvpn.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-client=${1-koizumi}
-set -- $SSH_CONNECTION
-server=${3-`curl ipv4.icanhazip.com`}
-
 docker pull kylemanna/openvpn
+
+server=${1-`docker run --rm -it kylemanna/openvpn wget -qO- ipv4.icanhazip.com`}
+client=${2-koizumi}
+
 docker run --name=ovpn_data -d --restart=always -v /root/openvpn:/etc/openvpn vaca/s6
 docker run --volumes-from=ovpn_data --rm kylemanna/openvpn ovpn_genconfig -u "$server"
 docker run --volumes-from=ovpn_data --rm -it kylemanna/openvpn ovpn_initpki
